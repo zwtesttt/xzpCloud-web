a1ee81e45ee45f209dca4f79ca163c6ceeb54fc3
minisailboat <minisailboat@qq.com>
2024-11-28 12:08:09 +0800

    fix: ConsoleLayout 修改

--- app/console/layout.tsx
+++ app/console/layout.tsx
@@ -9,16 +9,16 @@ export default function ConsoleLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex">
+    <div className="flex w-full">
       {/* 左变侧栏 */}
       <ConsoleSidebar />
       {/* 右内容 */}
-      <div className="top-16">
-        <div className="flex items-center gap-2 px-4 py-2">
+      <div className="flex-1 top-16">
+        <div className="flex flex-row justify-start items-center gap-2 px-4 py-2">
           <SidebarTrigger />
           {/* TODO：面包屑 */}
-          {children}
         </div>
+        {children}
       </div>
     </div>
   );
